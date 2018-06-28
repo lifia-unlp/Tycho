cafe63803b10a68286a7c0e32ccbc80501e1ea77
casco <alejandro.casco.fernandez@gmail.com>
2018-06-28 06:46:39 -0300

    Removed explanation text from BasicDemographicsComponent

--- web-ext/content_script/BasicDemographicsComponent.js
+++ web-ext/content_script/BasicDemographicsComponent.js
@@ -7,9 +7,7 @@ class BasicDemographicsComponent extends UIComponent {
     buildComponent() {
         let me = this;
         let welcome = $("<div id=\"welcome\" class=\"topNotification\"></div>");
-        welcome.append("<h2>Bienvenido al experimento</h2>");
-        welcome.append("<p>En la barra inferior encontrará las instrucciones.</p>");
-        welcome.append("<p>Le pediremos que realice una serie de tareas, y luego le presentaremos un formulario multiple choice para completar.</p>");
+        welcome.append("<h2>Cuéntenos sobre usted</h2>");
         welcome.append("<p class=\"field\"><label for=\"age\">Ingrese su edad</label><br/><input class=\"form-control\" id=\"age\" placeholder=\"Edad\"></p>");
         welcome.append("<p class=\"field\"><label for=\"hoursPerDay\">¿Cuántas hs por día promedio navega por internet?</label><br/><input class=\"form-control\" id=\"hoursPerDay\" placeholder=\"Nro\"></p>");
         welcome.append("<p><input id=\"welcome-button\"  disabled=\"\" type=\"submit\" class=\"tracker-btn\" value=\"Comenzar\"/></p>")
