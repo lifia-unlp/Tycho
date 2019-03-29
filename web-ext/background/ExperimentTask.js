9b328459bb741282cfa4e10434271980b0f01f97
casco <alejandro.casco.fernandez@gmail.com>
2019-03-29 19:00:14 -0300

    prepare for a potential return to a task with skip

--- web-ext/background/ExperimentTask.js
+++ web-ext/background/ExperimentTask.js
@@ -26,6 +26,12 @@ class ExperimentTask {
         if (this.componentClassname == 'SemaphoreSignalComponent') {
             BackgroundFacade.getSingleton().signalSemaphoreAndProceed(this.model.semaphoreId);
         }
+        if (this.componentClassname == 'TaskInstructionsComponent') {
+            // in case we come back with skip backwards to a started/finished task
+            this.model.paused = false;
+            this.model.ellapsedMs = 0;
+            this.model.startTime = null;
+        }     
     }
 
     static fromJson(taskJson) {
